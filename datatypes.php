e5ffcb8934331d154678157e9fdf3a312baedd7d
Anjali <anjalikesavarapu@gmail.com>
2022-04-29 15:56:24 +0530

    Datatypes and variables files added.

---
+++ datatypes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datatypes</title>
+</head>
+<body>
+    <?php
+        // String
+        $anytext = "Helloo,I'm any text";
+        // Integer
+        $integer = 10;
+        // Decimal Number
+        $decimal = 356.89
+        
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php 
+        $vehicleName = "cars";
+        $numberOfPeople = 20;
+        echo "There are $numberOfPeople people standin there.<br>";
+        echo "All the $vehicleName are going in different directions.";
+    ?>
+</body>
+</html>
