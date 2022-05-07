74348ee3c6f4880315b9834cb41bd5afeba83a66
Anjali <anjalikesavarapu@gmail.com>
2022-05-07 19:33:18 +0530

    updated calculator file with switch

--- simpleCalculator.php
+++ simpleCalculator.php
@@ -8,19 +8,35 @@
 </head>
 <body>
     
-    <form action="simpleCalculator.php" method="get">
-        Number 1: <input type="number" name="num1" />
-        <br>
-        <br>
-        Number 2: <input type="number" name="num2">
-        <br>
+    <form action="simpleCalculator.php" method="post">
+        Number 1 <input type="number" name="num1" /><br><br>
+        Number 2 <input type="number" name="num2"><br><br>
+        Operator <input type="text" name="op"> <br><br>
         <input type="submit" value="Submit" />
-
     </form>
-    <br>
 
     <?php
-        echo $_GET["num1"] +  $_GET["num2"];
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        switch($op) {
+            case '+':
+                echo $num1 + $num2;
+                break;
+            case '-':
+                echo $num1 - $num2;
+                break;
+            case '*':
+                echo $num1 * $num2;
+                break;
+            case '/':
+                echo $num1 / $num2;
+                break;
+            default:
+                echo "Enter valid operator";
+                break;
+        }
     ?>
 </body>
 </html>
