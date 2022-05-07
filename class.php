74c3d5630a88624d66b55dbc60b8aba473d8545b
Anjali <anjalikesavarapu@gmail.com>
2022-05-07 21:47:48 +0530

    Updated with object functions in cclass

--- class.php
+++ class.php
@@ -19,15 +19,20 @@
                 $this->author=$aAuthor;
                 $this->pages = $aPages;
             }
-            
+            //Object functions
+            function isBig() {
+                if($this->pages > 300) {
+                    return "true";
+                }
+                return "false";
+            }
         }
+
         // creating book object
         $book1 = new Book("Harry Potter", "JK Rowling", 400);
         $book2 = new Book("Lord of the rings", "Tolkien", 700);
-
-        
-
-
+        echo $book1->isBig(); 
+        echo $book2->isBig(); 
 
     ?>
 </body>
